4f39c9206d3a85ecf0ce08981b2deb967c08d4fc
panr99 <1547177202@qq.com>
2024-05-11 13:10:13 +0800

    添加初次相见相册

--- source/box/gallery/index.md
+++ source/box/gallery/index.md
@@ -1,6 +1,8 @@
 ---
 title: 画廊
-date: 2022-08-10 18:54:52
+date: 2024-5-11 13:07:45
 comments: false
 ---
-
+<div class="gallery-group-main">
+{% galleryGroup '初见' '初次相见' '/box/gallery/firstMeet' https://static.panrui.top/images/blog/gallery/file-169959676574915050522.jpg %}
+</div>
--- source/js/fomal.js
+++ source/js/fomal.js
@@ -3816,10 +3816,8 @@ if (localStorage.getItem("blogbg") != undefined) {
 } else {
   document.getElementById("defineBg").innerText = `:root{
     --default-bg: url(https://static.panrui.top/images/blog/pc_bg.jpg);
-    // --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
     --darkmode-bg:url(https://static.panrui.top/images/blog/pc_night_bg.jpg);
     --mobileday-bg: url(https://static.panrui.top/images/blog/mobile_bg.jpg);
-    // --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
     --mobilenight-bg: url(https://static.panrui.top/images/blog/mobile_night_bg.jpg);
   }`;
 }
